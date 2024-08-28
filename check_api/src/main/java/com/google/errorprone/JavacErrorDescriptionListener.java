74817f4ab539925fcc62ad6728e018f96aa1ac29
Frank Dana <ferdnyc@gmail.com>
2024-08-28 18:20:20 -0400

    Prevent duplicate remove-line messages
    Because multiple `appliedFixes` can have the `.isRemoveLine()` property,
    only suggest removing the line once per message construction.

--- check_api/src/main/java/com/google/errorprone/JavacErrorDescriptionListener.java
+++ check_api/src/main/java/com/google/errorprone/JavacErrorDescriptionListener.java
@@ -158,14 +158,19 @@ public class JavacErrorDescriptionListener implements DescriptionListener {
   private static String messageForFixes(Description description, List<AppliedFix> appliedFixes) {
     StringBuilder messageBuilder = new StringBuilder(description.getMessage());
     boolean first = true;
+    boolean suggestedRemoveLine = false;
     for (AppliedFix appliedFix : appliedFixes) {
+      if (appliedFix.isRemoveLine() && suggestedRemoveLine) {
+        continue;
+      }
       if (first) {
         messageBuilder.append("\nDid you mean ");
       } else {
         messageBuilder.append(" or ");
-      }
+      }        
       if (appliedFix.isRemoveLine()) {
         messageBuilder.append("to remove this line");
+        suggestedRemoveLine = true;
       } else {
         messageBuilder.append("'").append(appliedFix.getNewCodeSnippet()).append("'");
       }
